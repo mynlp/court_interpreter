1a96fe09f17d87c94ae32425e33c1577832b6b25
gifdog97 <omusubset.s@gmail.com>
2025-06-04 18:06:37 +0900

    enable prompt engineering, output metadata

--- court_interpreter/translate.py
+++ court_interpreter/translate.py
@@ -3,6 +3,7 @@
 
 import argparse
 import csv
+import string
 import uuid
 from logging import Logger
 
@@ -19,19 +20,22 @@ language_map = {
     "chinese": "中国語（簡体字）",
 }
 
-system_prompt = (
+DEFAULT_SYSTEM_PROMPT = (
     "あなたは法廷通訳を行う通訳士です。法廷におけるやり取りを正確に翻訳してください。"
 )
 
+DEFAULT_USER_PROMPT = string.Template(
+    "以下の日本語の文を${language}に翻訳してください。"
+    "意訳は避け、単語の省略や付加をしない逐語訳で訳すようにしてください。"
+    "否定疑問文、付加疑問文、修辞疑問文はニュアンスが変わらないように注意して訳してください。"
+    "回答は必ず一行になるようにして下さい。\n"
+    "${sentence}"
+)
 
-def translate_with_client(client, model: str, language: str, sentence: str) -> str:
-    user_prompt = (
-        f"以下の日本語の文を{language_map[language]}に翻訳してください。"
-        f"意訳は避け、単語の省略や付加をしない逐語訳で訳すようにしてください。"
-        f"疑問文はニュアンスが変わらないように注意して訳してください。"
-        "回答は必ず一行になるようにして下さい。\n"
-        f"{sentence}\n"
-    )
+
+def translate_with_client(
+    client, model: str, system_prompt: str, user_prompt: str
+) -> str:
     completion = client.chat.completions.create(
         model=model,
         messages=[
@@ -47,14 +51,18 @@ def translate_with_gpt(
     api_key: str,
     source_sentences: list[str],
     language: str,
+    system_prompt: str,
+    user_prompt: string.Template,
     logger=None,
 ) -> list[str]:
     client = OpenAI(organization=organization, api_key=api_key)
-
     translated_dataset: list[str] = []
     for sentence in tqdm(source_sentences):
+        user_prompt_str = user_prompt.substitute(
+            language=language_map[language], sentence=sentence
+        )
         translated_dataset.append(
-            translate_with_client(client, "gpt-4o", language, sentence)
+            translate_with_client(client, "gpt-4o", system_prompt, user_prompt_str)
         )
     return translated_dataset
 
@@ -63,13 +71,20 @@ def translate_with_llama(
     api_key: str,
     source_sentences: list[str],
     language: str,
+    system_prompt: str,
+    user_prompt: string.Template,
     logger=None,
 ) -> list[str]:
     client = Groq(api_key=api_key)
     translated_dataset: list[str] = []
     for sentence in tqdm(source_sentences):
+        user_prompt_str = user_prompt.substitute(
+            language=language_map[language], sentence=sentence
+        )
         translated_dataset.append(
-            translate_with_client(client, "llama-3.1-70b-versatile", language, sentence)
+            translate_with_client(
+                client, "llama-3.1-70b-versatile", system_prompt, user_prompt_str
+            )
         )
     return translated_dataset
 
@@ -106,41 +121,29 @@ def translate_with_azure(
     return english_dataset, chinese_dataset, vietnamese_dataset
 
 
-def main(args: argparse.Namespace, logger: Logger) -> None:
-    sentences_map: dict[str, list[str]] = {}
-    # Load dataset (tsv)
-    with open(args.dataset_path) as f:
-        dataset = csv.DictReader(f, delimiter="\t")
-        ids, source_sentences = [], []
-        for row in dataset:
-            ids.append(row["id"])
-            source_sentences.append(row["source_japanese"])
-        sentences_map["japanese"] = source_sentences
-    logger.info(f"Dataset size (Japanese)\t: {len(source_sentences)}")
-    logger.info("Start translation")
-    if args.system == "gpt" or args.system == "llama":
-        for language in ["english", "chinese", "vietnamese"]:
-            if args.system == "gpt":
-                translated_sentences = translate_with_gpt(
-                    args.gpt_organization, args.api_key, source_sentences, language
-                )
-            elif args.system == "llama":
-                translated_sentences = translate_with_llama(
-                    args.api_key, source_sentences, language
-                )
-            logger.info(
-                f"Translated ({language.capitalize()})\t: {len(translated_sentences)}"
-            )
-            assert len(source_sentences) == len(translated_sentences)
-            sentences_map[language] = translated_sentences
-    elif args.system == "azure":
-        english, chinese, vietnamese = translate_with_azure(
-            args.api_key, source_sentences
-        )
-        sentences_map["english"] = english
-        sentences_map["chinese"] = chinese
-        sentences_map["vietnamese"] = vietnamese
+def parse_prompt_file(prompt_file: str) -> tuple[str, string.Template]:
+    """
+    prompt_file format is as follows:
+        system_prompt:
+        ...
+        user_prompt:
+        ...
+    """
+    with open(prompt_file, "r") as f:
+        lines = f.readlines()
+    i = 1
+    system_prompt = ""
+    while not lines[i].startswith("user_prompt:"):
+        system_prompt += lines[i].strip()
+        i += 1
+    user_prompt = "以下の日本語の文を${language}に翻訳してください。"
+    user_prompt += "".join(lines[i + 1 :]).strip().replace("\n", "")
+    user_prompt += "\n"
+    user_prompt += "${sentence}"
+    return system_prompt, string.Template(user_prompt)
 
+
+def summarize_results(sentences_map, ids):
     results = []
     for id, ja, en, zh, vi in zip(
         ids,
@@ -158,7 +161,56 @@ def main(args: argparse.Namespace, logger: Logger) -> None:
                 "vietnamese": vi,
             }
         )
-    with open(args.csv_path, "w") as f:
+    return results
+
+
+def main(args: argparse.Namespace, logger: Logger) -> None:
+    sentences_map: dict[str, list[str]] = {}
+    # Load dataset (tsv)
+    with open(args.input_path) as f:
+        dataset = csv.DictReader(f, delimiter="\t")
+        ids, source_sentences = [], []
+        for row in dataset:
+            ids.append(row["id"])
+            source_sentences.append(row["source_japanese"])
+        sentences_map["japanese"] = source_sentences
+    logger.info(f"Dataset size (Japanese)\t: {len(source_sentences)}")
+    if args.prompt_file:
+        system_prompt, user_prompt = parse_prompt_file(args.prompt_file)
+    else:
+        system_prompt, user_prompt = DEFAULT_SYSTEM_PROMPT, DEFAULT_USER_PROMPT
+    if args.system == "gpt" or args.system == "llama":
+        for language in ["english", "chinese", "vietnamese"]:
+            logger.info(f"Start translating {language.capitalize()}")
+            if args.system == "gpt":
+                translated_sentences = translate_with_gpt(
+                    args.gpt_organization,
+                    args.api_key,
+                    source_sentences,
+                    language,
+                    system_prompt,
+                    user_prompt,
+                )
+            elif args.system == "llama":
+                translated_sentences = translate_with_llama(
+                    args.api_key, source_sentences, language, system_prompt, user_prompt
+                )
+            logger.info(
+                f"Translated ({language.capitalize()})\t: {len(translated_sentences)}"
+            )
+            assert len(source_sentences) == len(translated_sentences)
+            sentences_map[language] = translated_sentences
+    elif args.system == "azure":
+        english, chinese, vietnamese = translate_with_azure(
+            args.api_key, source_sentences
+        )
+        sentences_map["english"] = english
+        sentences_map["chinese"] = chinese
+        sentences_map["vietnamese"] = vietnamese
+
+    results = summarize_results(sentences_map, ids)
+    # Write results
+    with open(args.output_path, "w") as f:
         f.write(
             f"id\tsource_japanese\t{args.system}_english\t{args.system}_chinese\t{args.system}_vietnamese\n"
         )
@@ -166,16 +218,30 @@ def main(args: argparse.Namespace, logger: Logger) -> None:
             f.write(
                 f"{data['id']}\t{data['japanese']}\t{data['english']}\t{data['chinese']}\t{data['vietnamese']}\n"
             )
+    # Write metadata
+    with open(f"{args.output_path}_metadata", "w") as f:
+        f.write(f"system: {args.system}\n\n")
+        if args.system != "azure":
+            f.write(f"system_prompt:\n{system_prompt}\n\n")
+            f.write(f"user_prompt:\n{user_prompt.template}")
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--gpt_organization")
     parser.add_argument("--api_key")
-    parser.add_argument("--dataset_path", default="../dataset/handbook_filtered.tsv")
-    parser.add_argument("--system", default="gpt", choices=["gpt", "llama", "azure"])
     parser.add_argument(
-        "--csv_path", default="../output/translation/handbook_filtered-gpt.tsv"
+        "--prompt_file",
+        help="Path to the prompt file. If not provided, a default prompt will be used.",
+    )
+    parser.add_argument(
+        "--input_path", required=True, default="../dataset/handbook_filtered.tsv"
+    )
+    parser.add_argument(
+        "--system", required=True, default="gpt", choices=["gpt", "llama", "azure"]
+    )
+    parser.add_argument(
+        "--output_path", required=True, help="Path to save the translated dataset"
     )
     args = parser.parse_args()
     logger = get_logger()
