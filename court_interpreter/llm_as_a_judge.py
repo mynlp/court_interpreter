7d45294e330662963c0d3f661818de4f3680b43a
gifdog97 <omusubset.s@gmail.com>
2025-10-14 15:02:14 +0900

    update llm_as_a_judge, execute and remap again

--- court_interpreter/llm_as_a_judge.py
+++ court_interpreter/llm_as_a_judge.py
@@ -13,9 +13,15 @@ from tqdm import tqdm
 
 from court_interpreter.utils import get_logger
 
+# NLP2025 version
+# source_map = {
+#     "english": "中国語（簡体字）",
+#     "vietnamese": "英語",
+#     "chinese": "日本語",
+# }
 source_map = {
-    "english": "中国語（簡体字）",
-    "vietnamese": "英語",
+    "english": "日本語",
+    "vietnamese": "日本語",
     "chinese": "日本語",
 }
 language_map = {
@@ -38,14 +44,14 @@ def create_system_prompt(
         "それぞれの翻訳性能を評価してください。\n"
         "評価は以下の四つの基準に従って行ってください。\n"
         "1. 省略 (Omission)\n"
-        "翻訳された文中において、原文にある情報が欠落しているかどうかを評価します。\n"
+        "翻訳された文中において、原文にある情報が欠落しているかどうかを評価します。"
         "単語レベルで訳の省略があるか否かについて評価を行って下さい。\n"
         "評価基準: \n"
         "0：単語レベルでの省略がある。\n"
-        "1：単語レベルでの省略がない。"
-        "2. 付加 (Addition)\n"
+        "1：単語レベルでの省略がない。\n"
         "\n"
-        "翻訳された文中において、原文にない情報が付加されているかどうかを評価します。\n"
+        "2. 付加 (Addition)\n"
+        "翻訳された文中において、原文にない情報が付加されているかどうかを評価します。"
         "単語レベルでの訳の付加があるか否かについて評価を行って下さい。\n"
         f"{note}"
         "評価基準: \n"
@@ -53,16 +59,28 @@ def create_system_prompt(
         "1：単語レベルでの付加がない。\n"
         "\n"
         "3. 単語の意味 (Word Meaning)\n"
-        "省略や付加がされていない各単語やフレーズについて、"
-        "それぞれ適切に翻訳されているか、意味が変わっていないかを評価します。"
-        "ただし、文全体として誤訳になっている場合も「0」と評価して下さい。\n"
+        "省略や付加がされていない各単語やフレーズについて、それぞれ適切に翻訳されているか、意味が変わっていないかを評価します。"
+        "ただし、文全体として誤訳になっている場合も「1」と評価して下さい。"
+        "・他の言語の文字が含まれている場合は、誤訳を含んでいるとみなして「1」として下さい。\n"
+        "例：\n"
+        "《英文に日本語が含まれている場合》\n"
+        "In that case, please <最高裁判所>submit a petition of appeal to the XX High Court within 14 days from tomorrow<上告>.\n"
+        "《ベトナム語に中国語が含まれている場合》\n"
+        "Tôi không đồng ý với biên bản lời khai của nhân chứng A về lời khai với công tố viên và cảnh sát tư pháp về tài liệu 甲3号证 và 甲4号证.\n"
+        "・記号が他の記号に置換されている場合は考慮しないで下さい（減点対象ではない）。\n"
+        "例：\n"
+        "《原文》私は ______ 地方裁判所の裁判官です。\n"
+        "《翻訳文》Tôi là thẩm phán của Tòa án Quận XX.\n"
         "評価基準: \n"
-        "0：単語の意味の誤りがある。または、文全体として誤訳になっている。\n"
-        "1：単語の意味が誤りがない。\n"
+        "1. 間違っている、文全体として誤訳となっている（意味が正しく伝わらない）\n"
+        "2. 原文とは異なる意味に解釈される可能性があり不適切（意味の拡大、縮小）\n"
+        "3. 正しく意味が伝わる可能性が高いが、より適切な単語を使うべきである\n"
+        "4. 正しく意味が伝わる可能性が高いが、自分ならこのようには訳さない（間違ってはいない）\n"
+        "5. 問題を感じない（自分が訳す時もこのように訳す）\n"
         "\n"
         "4. 流暢性 (Fluency)\n"
-        "翻訳された文が文法的および自然な言葉で表現され、読みやすいかどうかを評価します。\n"
-        "ただし、翻訳内容の正しさは考慮せず、純粋に文自体の流暢性を評価して下さい。\n"
+        "翻訳された文が文法的および自然な言葉で表現され、読みやすいかどうかを評価します。"
+        "ただし、翻訳内容の正しさは考慮せず、純粋に文自体の流暢性を評価して下さい。"
         "他の言語の文字が含まれている場合は、流暢性が低いとみなして減点して下さい。\n"
         "評価基準: \n"
         "1：著しく不自然で理解しにくい。\n"
@@ -94,33 +112,37 @@ def judge(
     if question:
         question_prompt = (
             "5. 質問 (Question)\n"
-            "翻訳された文が原文に対して適切な質問を引き起こすかどうかを評価します。\n"
-            "翻訳された文が原文に対して適切な質問を引き起こすか否かについて評価を行って下さい。\n"
+            "翻訳された文が原文の疑問文のニュアンスを適切に再現しているかを評価します。"
+            "ニュアンスがそのまま保持され、発言者の意図が正確に伝わっていることを確認します。\n"
             "評価基準: \n"
-            "0：翻訳された文が原文に対して適切な質問を引き起こさない。\n"
-            "1：翻訳された文が原文に対して適切な質問を引き起こす。\n"
+            "1. 間違っている（発言者の意図が正しく伝わらない）\n"
+            "2. 正しく意味が伝わる可能性が高いが、より適切な表現がある\n"
+            "3. 問題を感じない（自分が訳す時もこのように訳す）\n"
+            "\n"
         )
         score_format = (
             "id,"
-            "Aのomissionスコア,Aのadditionスコア,Aのword_meaningスコア,AのQuestionスコア,Aのfluencyスコア"
-            "Bのomissionスコア,Bのadditionスコア,Bのword_meaningスコア,BのQuestionスコア,Bのfluencyスコア"
-            "Cのomissionスコア,Cのadditionスコア,Cのword_meaningスコア,CのQuestionスコア,Cのfluencyスコア\n"
-            "例：handbook001,1,0,1,1,5,1,1,1,0,4,0,0,1,1,5\n"
+            "Aのomissionスコア,Aのadditionスコア,Aのword_meaningスコア,Aのfluencyスコア,AのQuestionスコア,"
+            "Bのomissionスコア,Bのadditionスコア,Bのword_meaningスコア,Bのfluencyスコア,BのQuestionスコア,"
+            "Cのomissionスコア,Cのadditionスコア,Cのword_meaningスコア,Cのfluencyスコア,CのQuestionスコア\n"
+            "例：handbook001,1,0,4,5,2,1,1,3,4,1,0,0,2,3,1\n"
         )
+        score_num = 16
     else:
         question_prompt = ""
         score_format = (
             "id,"
-            "Aのomissionスコア,Aのadditionスコア,Aのword_meaningスコア,Aのfluencyスコア"
-            "Bのomissionスコア,Bのadditionスコア,Bのword_meaningスコア,Bのfluencyスコア"
-            "Cのomissionスコア,Cのadditionスコア,Cのword_meaningスコア,Cのfluencyスコア"
+            "Aのomissionスコア,Aのadditionスコア,Aのword_meaningスコア,Aのfluencyスコア,"
+            "Bのomissionスコア,Bのadditionスコア,Bのword_meaningスコア,Bのfluencyスコア,"
+            "Cのomissionスコア,Cのadditionスコア,Cのword_meaningスコア,Cのfluencyスコア,"
             "Dのomissionスコア,Dのadditionスコア,Dのword_meaningスコア,Dのfluencyスコア\n"
-            "例：handbook001,1,0,1,5,1,1,1,4,0,0,1,5,1,1,1,5\n"
+            "例：handbook001,1,0,5,5,1,1,3,4,0,0,2,3,1,1,4,2\n"
         )
+        score_num = 17
     system_prompt = create_system_prompt(
         source_language, translation_language, question_prompt, score_format
     )
-    # logger.info(system_prompt)
+    logger.info(system_prompt)
     for i in tqdm(range(len(ids))):
         id = ids[i]
         s = sources[i]
@@ -139,35 +161,69 @@ def judge(
                         {"role": "user", "content": user_prompt},
                     ],
                 )
+                score_str = completion.choices[0].message.content.strip()
+                assert len(score_str.split(",")) == score_num
                 break
             except APIConnectionError:
                 time.sleep(2)  # avoid rate limit error
-        scores.append(completion.choices[0].message.content.strip())
+            except AssertionError:
+                logger.warning(f"Invalid score format. {score_str}")
+                logger.warning(f"Retrying... {_ + 1}/5")
+                time.sleep(2)
+            if _ == 4:
+                raise ValueError("Failed to get valid score after 5 retries.")
+        scores.append(score_str)
     return scores
+
+
+def normalize_score(score: str, question: bool) -> str:
+    def _normalize_handbook_score(scores: list[str]) -> list[str]:
+        scores[2] = str((int(scores[2]) - 1) / 4)  # word_meaning 1-5 -> 0-1
+        scores[3] = str((int(scores[3]) - 1) / 4)  # fluency 1-5 -> 0-1
+        return scores
+
+    def _normalize_question_score(scores: list[str]) -> list[str]:
+        scores[2] = str((int(scores[2]) - 1) / 4)  # word_meaning 1-5 -> 0-1
+        scores[3] = str((int(scores[3]) - 1) / 4)  # fluency 1-5 -> 0-1
+        scores[4] = str((int(scores[4]) - 1) / 2)  # question 1-3 -> 0-1
+        return scores
+
+    parts = score.split(",")
+    if question:
+        id = parts[0]
+        A_scores = parts[1:6]
+        B_scores = parts[6:11]
+        C_scores = parts[11:16]
+        return f"{id},{','.join(_normalize_question_score(A_scores))},{','.join(_normalize_question_score(B_scores))},{','.join(_normalize_question_score(C_scores))}"
+    else:
+        id = parts[0]
+        A_scores = parts[1:5]
+        B_scores = parts[5:9]
+        C_scores = parts[9:13]
+        D_scores = parts[13:17]
+        return f"{id},{','.join(_normalize_handbook_score(A_scores))},{','.join(_normalize_handbook_score(B_scores))},{','.join(_normalize_handbook_score(C_scores))},{','.join(_normalize_handbook_score(D_scores))}"
 
 
 def write_scores(scores: list[str], evaluation_path: str, question: bool) -> None:
     if question:
         columns = (
-            "id,omission_A,addition_A,word_meaning_A,question_A,fluency_A,"
-            "omission_B,addition_B,word_meaning_B,question_B,fluency_B,"
-            "omission_C,addition_C,word_meaning_C,question_C,fluency_C\n"
+            "id,omission_A,addition_A,word_meaning_A,fluency_A,question_A,"
+            "omission_B,addition_B,word_meaning_B,fluency_B,question_B,"
+            "omission_C,addition_C,word_meaning_C,fluency_C,question_C\n"
         )
-        score_num = 16
     else:
         columns = (
             "id,omission_A,addition_A,word_meaning_A,fluency_A,omission_B,addition_B,word_meaning_B,fluency_B,"
             "omission_C,addition_C,word_meaning_C,fluency_C,omission_D,addition_D,word_meaning_D,fluency_D\n"
         )
-        score_num = 17
     with open(evaluation_path, "w") as f:
         f.write(columns)
         for score in scores:
-            assert len(score.split(",")) == score_num
-            f.write(f"{score}\n")
+            f.write(f"{normalize_score(score, question)}\n")
 
 
 def main(args: argparse.Namespace, logger: Logger) -> None:
+    logger.info(args)
     evaluation_path = f"{args.output_dir}/{Path(args.evaluation_set_path).name.replace('.tsv', '.csv')}"
     df = pd.read_csv(args.evaluation_set_path, sep="\t")
     # id  source  translation_A	translation_B	translation_C	translation_D	mapping
